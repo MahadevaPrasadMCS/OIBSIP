0447de71ce31d3ca043cf99e0553874b846074ff
Mahadeva Prasad M <mahadevaprasdacs223@gmail.com>
2025-10-17 23:58:40 +0530

    Started new task

---
+++ Task2/src/WelcomeFrame.java
@@ -0,0 +1,11 @@
+Package Task2;
+
+import java.swing.*;
+import awt.ActionEvent;
+
+public class WelcomeFrame extends JFrame{
+    public static void main(String[] args) {
+        WelcomeFrame frame1 = new WelcomeFrame();
+        frame1.setVisible(true);
+    }
+}
